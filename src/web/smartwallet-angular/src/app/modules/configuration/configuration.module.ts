530ea6907aa8181e05c43b3503d64fc2b214738b
vbondaryuk <vbondaryuk@hotmail.com>
2019-07-23 01:16:11 +0300

    Added category tree view component

--- src/web/smartwallet-angular/src/app/modules/configuration/configuration.module.ts
+++ src/web/smartwallet-angular/src/app/modules/configuration/configuration.module.ts
@@ -4,13 +4,15 @@ import {CommonModule} from '@angular/common';
 import {ConfigurationRoutingModule} from './configuration-routing.module';
 import {ConfigurationComponent} from './components/configuration/configuration.component';
 import {FormsModule} from '@angular/forms';
-import {MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatFormFieldModule, MatInputModule, MatTreeModule, MatIconModule} from '@angular/material';
 import {MatSelectModule} from '@angular/material';
 import {SharedModule} from '../../shared/shared.module';
+import {CategoryTreeviewComponent} from './components/category.treeview/category.treeview.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
-  declarations: [ConfigurationComponent],
+  declarations: [ConfigurationComponent, CategoryTreeviewComponent],
   imports: [
     CommonModule,
     ConfigurationRoutingModule,
@@ -18,7 +20,10 @@ import {SharedModule} from '../../shared/shared.module';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    SharedModule
+    SharedModule,
+    MatTreeModule,
+    MatIconModule,
+    FlexLayoutModule
   ]
 })
 export class ConfigurationModule {
